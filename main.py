b0a3416c96dcf84bc3ce466bd5701cf39ce59d29
Simon Swart <simonswart91@gmail.com>
2025-06-30 18:49:24 +0200

    First and this is fun.......

---
+++ main.py
@@ -0,0 +1,21 @@
+# Text game
+
+# Text prompts (in order of the story)
+intro1 = "You wake up at the entrance of a dark wailing forest"
+intro2 = "You look behind you and see a thick fog setting, leaving you with no choice but to enter the forest"
+trav = "As you enter you are greeted by Thorgus the traveling merchant"
+throgus1 = "Greetings lost one! I haven't seen you around these parts before..."
+
+
+# Input
+name = input("What do they call you? ")
+
+# Now that we have the name, we can format the next line
+throgus2 = f"Ahh so they call you {name}? I haven't heard that name before..."
+
+# Print sequence
+print(intro1)
+print(intro2)
+print(trav)
+print(throgus1)
+print(throgus2)
